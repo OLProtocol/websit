62e1f8580079c4d507305ebe43974e0845f773a4
Hugh <ygz14835187@163.com>
2024-01-02 17:43:06 +0800

    change local order style

--- src/pages/inscribe/components/LocalOrderList.tsx
+++ src/pages/inscribe/components/LocalOrderList.tsx
@@ -1,49 +1,69 @@
+import { useMemo } from 'react';
 import { useOrderStore, OrderItemType } from '@/store';
-import {
-  Table,
-  Thead,
-  Tbody,
-  Tfoot,
-  Tr,
-  Th,
-  Td,
-  TableCaption,
-  TableContainer,
-} from '@chakra-ui/react';
+import { Space, Table, Tag } from 'antd';
+import type { ColumnsType } from 'antd/es/table';
 import { hideStr } from '@/lib/utils';
 import { useUnisatConnect } from '@/lib/hooks/unisat';
 import { inscribe } from '../utils';
+import { render } from 'react-dom';
 
 interface LocalOrderListProps {
   onOrderClick?: (item: OrderItemType) => void;
 }
+interface DataType {
+  orderId: string;
+  status: string;
+  created: number;
+}
 export const LocalOrderList = ({ onOrderClick }: LocalOrderListProps) => {
   const { list } = useOrderStore((state) => state);
+  const clickHandler = ({ orderId }) => {
+    const item = list.find((v) => v.orderId === orderId);
+    if (item) {
+      onOrderClick?.(item);
+    }
+  };
+  const columns: ColumnsType<DataType> = [
+    {
+      title: 'Order Id',
+      dataIndex: 'orderId',
+      key: 'orderId',
+      render: (_, { orderId }) => {
+        return hideStr(orderId, 10);
+      },
+    },
+    {
+      title: 'Status',
+      dataIndex: 'status',
+      key: 'status',
+    },
+    {
+      title: 'Create Date',
+      dataIndex: 'created',
+      key: 'created',
+      render: (_, { created }) => {
+        return new Date(created).toLocaleString();
+      },
+    },
+  ];
+  const dataSource: DataType[] = useMemo(
+    () =>
+      list.map((item) => ({
+        orderId: item.orderId,
+        status: item.status,
+        created: item.createAt,
+      })),
+    [list],
+  );
   return (
-    <TableContainer>
-      <Table variant='simple'>
-        <TableCaption>Local Order List</TableCaption>
-        <Thead>
-          <Tr>
-            <Th>Order Id</Th>
-            <Th>Status</Th>
-            <Th>Create Date</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {list.map((item) => (
-            <Tr
-              key={item.orderId}
-              onClick={() => {
-                onOrderClick?.(item);
-              }}>
-              <Td>{hideStr(item.orderId, 10)}</Td>
-              <Td>{item.status}</Td>
-              <Td>{new Date(item.createAt).toLocaleString()}</Td>
-            </Tr>
-          ))}
-        </Tbody>
-      </Table>
-    </TableContainer>
+    <Table
+      columns={columns}
+      dataSource={dataSource}
+      onRow={(record) => {
+        return {
+          onClick: () => clickHandler(record), // 点击行
+        };
+      }}
+    />
   );
 };
