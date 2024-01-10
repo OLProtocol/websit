9da4053e0f50359e4928cf3fb89d7ba11bd781f0
Hugh <ygz14835187@163.com>
2024-01-10 14:17:54 +0800

    add minted

--- src/pages/ordx/components/OrdxFullList.tsx
+++ src/pages/ordx/components/OrdxFullList.tsx
@@ -22,21 +22,61 @@ export const Ord2FullList = () => {
       title: 'Tick',
       dataIndex: 'tick',
       key: 'tick',
+      fixed: 'left',
+      width: 100,
+      align: 'center',
     },
     {
       title: 'Deploy Time',
       dataIndex: 'deploy_time',
       key: 'deploy_time',
+      width: 200,
+      align: 'center',
+    },
+    {
+      title: 'Description',
+      dataIndex: 'description',
+      key: 'description',
+      width: 200,
+      align: 'center',
+      render: (t) => t || '-'
+    },
+    {
+      title: 'Limit',
+      dataIndex: 'limit',
+      key: 'limit',
+      width: 100,
+      align: 'center',
     },
     {
       title: 'Block',
       dataIndex: 'block',
       key: 'block',
+      width: 200,
+      align: 'center',
+    },
+    {
+      title: 'Deploy Height',
+      dataIndex: 'deployHeight',
+      key: 'deployHeight',
+      width: 150,
+      align: 'center',
+    },
+
+    {
+      title: 'Regular Expression',
+      dataIndex: 'reg',
+      key: 'reg',
+      width: 200,
+      align: 'center',
+      render: (t) => t || '-'
     },
     {
       title: 'Rarity',
       dataIndex: 'rarity',
       key: 'rarity',
+      width: 100,
+      align: 'center',
       render: (rarity) => {
         return (
           <Tag color='green' key={rarity}>
@@ -46,9 +86,19 @@ export const Ord2FullList = () => {
       },
     },
     {
-      title: 'Holders',
+      title: 'Max Supply',
       dataIndex: 'holders',
-      key: 'holders',
+      key: 'supply',
+      width: 100,
+      align: 'center',
+    },
+    {
+      title: 'Minted',
+      dataIndex: 'minted',
+      key: 'minted',
+      fixed: 'right',
+      width: 100,
+      align: 'center',
     },
   ];
   const dataSource: DataType[] = useMemo(
@@ -57,7 +107,12 @@ export const Ord2FullList = () => {
         tick: item.ticker,
         block: `${item.startBlock}-${item.endBlock}`,
         rarity: item.rarity,
+        description: item.description,
+        reg: item.reg,
         holders: item.holdersCount,
+        deployHeight: item.deployHeight,
+        minted: item.totalMinted,
+        limit: item.limit,
         deploy_time: new Date(item.deployBlocktime).toLocaleString(),
       })),
     [list],
@@ -68,7 +123,11 @@ export const Ord2FullList = () => {
         The full list of ordx
       </h2>
       <div className='flex mb-4 justify-center'>
-        <Segmented options={['All', 'In-Progress', 'Completed']} block className='w-80' />
+        <Segmented
+          options={['All', 'In-Progress', 'Completed']}
+          block
+          className='w-80'
+        />
       </div>
       <Table
         columns={columns}
@@ -76,6 +135,7 @@ export const Ord2FullList = () => {
         pagination={{
           position: ['bottomCenter'],
         }}
+        scroll={{ x: 1450 }}
         onRow={(record) => {
           return {
             onClick: () => clickHandler(record), // 点击行
