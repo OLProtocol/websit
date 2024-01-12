d8949bfcd5a0ae13d0a124edc1d571dcf9db3cc9
Hugh <ygz14835187@163.com>
2024-01-12 16:03:27 +0800

    add home banner

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,24 +1,13 @@
-import {
-  SimpleGrid,
-  Card,
-  CardHeader,
-  Image,
-  Box,
-  CardBody,
-  Heading,
-  Stack,
-  ListItem as Litem,
-  StackDivider,
-  Input,
-  InputGroup,
-  InputRightElement,
-} from '@chakra-ui/react';
+import HomeBannerImage from '@/assets/images/home_banner.jpg';
 
 export default function Home() {
-
   return (
-    <div className='h-full overflow-y-auto flex flex-col'>
-
+    <div className="min-h-full bg-gray-900">
+      <div className='max-w-3xl mx-auto pt-4'>
+        <div className='rounded-2xl overflow-hidden'>
+          <img src={HomeBannerImage} alt='banner' className='w-full' />
+        </div>
+      </div>
     </div>
   );
 }
