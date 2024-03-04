fda543125ca192738d39974376a075e941cbefd2
Hugh <ygz14835187@163.com>
2024-03-04 15:05:36 +0800

    merge dev

--- src/pages/tools/index.tsx
+++ src/pages/tools/index.tsx
@@ -5,20 +5,27 @@ import { BtcHeightAlert } from '@/components/BtcHeightAlert';
 import { Card, Input } from 'antd';
 import { useState } from 'react';
 import { getMintInfo } from '@/api';
-import { useNavigate } from 'react-router-dom';
+import { ROUTE_PATH } from '@/router';
 
 export default function InscribeCheck() {
   const nav = useNavigate();
   const { t } = useTranslation();
-
+  const toSplitSats = () => {
+    nav(ROUTE_PATH.TOOLS_SPLIT_SATS);
+  };
   const toSearchRareSat = () => {
     nav('/tools/rare_sat');
   };
 
   return (
     <div className='flex gap-2 max-w-xl mx-auto p-2'>
-      <Card title='Split Sats' className='w-60'>Split a range of sats into segments.</Card>
-      <Card title={t('pages.rare_sat.title')} className='w-60 cursor-pointer' onClick={toSearchRareSat}>
+      <Card title='Split Sats' className='w-60' onClick={toSplitSats}>
+        Split a range of sats into segments.
+      </Card>
+      <Card
+        title={t('pages.rare_sat.title')}
+        className='w-60 cursor-pointer'
+        onClick={toSearchRareSat}>
         {t('pages.rare_sat.des')}
       </Card>
     </div>
