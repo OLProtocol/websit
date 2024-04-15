26461a646af546e4f51d21b45b38da9b8225cca7
duyouyou <duyouyou88@gmail.com>
2024-04-15 17:29:56 +0800

    tools

--- src/pages/tools/index.tsx
+++ src/pages/tools/index.tsx
@@ -21,15 +21,21 @@ export default function InscribeCheck() {
 
   return (
     <div className='flex gap-2 max-w-max mx-auto p-2'>
-      <Card title={t('pages.tools.splitted_inscription.title')} className='w-60 cursor-pointer' onClick={toSplittedInscriptions}>
+      {['ordx.space'].every((v) => location.hostname !== v) && (
+        <Card title={t('pages.tools.splitted_inscription.title')} className='w-60 cursor-pointer' onClick={toSplittedInscriptions}>
         {t('pages.tools.splitted_inscription.des')}
       </Card>
+      )}
+      
       <Card title={t('pages.tools.utxo.title')} className='w-60 cursor-pointer' onClick={toUtxo}>
         {t('pages.tools.utxo.des')}
       </Card>
+
+      {['ordx.space'].every((v) => location.hostname !== v) && (
       <Card title='Transaction' className='w-60 cursor-pointer' onClick={toTransact}>
         发送和拆分的工具。
       </Card>
+       )}
     </div>
   );
 }
