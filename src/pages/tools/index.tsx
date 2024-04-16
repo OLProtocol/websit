53b1c91a34759267ecdd55186d37394e51b486ac
duyouyou <duyouyou88@gmail.com>
2024-04-16 16:56:33 +0800

    bug 92

--- src/pages/tools/index.tsx
+++ src/pages/tools/index.tsx
@@ -2,6 +2,7 @@ import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { Card } from 'antd';
 import { ROUTE_PATH } from '@/router';
+import { SimpleGrid } from '@chakra-ui/react';
 
 export default function InscribeCheck() {
   const nav = useNavigate();
@@ -19,23 +20,33 @@ export default function InscribeCheck() {
     nav(ROUTE_PATH.TOOLS_TRANSACT, { state: { initInputList: [], initOutputList: [] } });
   }
 
+  const toSat = () => {
+    nav(ROUTE_PATH.TOOLS_SEARCH_SAT);
+  }
+
   return (
     <div className='flex gap-2 max-w-max mx-auto p-2'>
-      {['ordx.space'].every((v) => location.hostname !== v) && (
-        <Card title={t('pages.tools.splitted_inscription.title')} className='w-60 cursor-pointer' onClick={toSplittedInscriptions}>
-        {t('pages.tools.splitted_inscription.des')}
-      </Card>
-      )}
-      
-      <Card title={t('pages.tools.utxo.title')} className='w-60 cursor-pointer' onClick={toUtxo}>
-        {t('pages.tools.utxo.des')}
-      </Card>
+      <SimpleGrid columns={3} spacing={10}>
+        {['ordx.space'].every((v) => location.hostname !== v) && (
+          <Card title={t('pages.tools.splitted_inscription.title')} className='w-60 cursor-pointer' onClick={toSplittedInscriptions}>
+            {t('pages.tools.splitted_inscription.des')}
+          </Card>
+        )}
 
-      {['ordx.space'].every((v) => location.hostname !== v) && (
-      <Card title='Transaction' className='w-60 cursor-pointer' onClick={toTransact}>
-        发送和拆分的工具。
-      </Card>
-       )}
-    </div>
+        <Card title={t('pages.tools.utxo.title')} className='w-60 cursor-pointer' onClick={toUtxo}>
+          {t('pages.tools.utxo.des')}
+        </Card>
+
+        {['ordx.space'].every((v) => location.hostname !== v) && (
+          <Card title={t('pages.tools.transaction.title')} className='w-60 cursor-pointer' onClick={toTransact}>
+            {t('pages.tools.transaction.des')}
+          </Card>
+        )}
+
+          <Card title={t('pages.tools.sat.title')} className='w-60 cursor-pointer' onClick={toSat}>
+            {t('pages.tools.sat.des')}
+          </Card>
+        </SimpleGrid>
+      </div>
   );
 }
