015e9286201fee975f2e9eaa7f974d834af930b0
duyouyou <duyouyou88@gmail.com>
2024-05-13 15:42:56 +0800

    toolbox

--- src/layout/NavHeader.tsx
+++ src/layout/NavHeader.tsx
@@ -63,12 +63,12 @@ export const NavHeader = () => {
           : 'https://docs.ordx.space/',
       type: 'link',
     },
-    {
-      key: ROUTE_PATH.TOOLS,
-      label: t('nav.tools'),
-      value: ROUTE_PATH.TOOLS,
-      type: 'route',
-    },
+    // {
+    //   key: ROUTE_PATH.TOOLS,
+    //   label: t('nav.tools'),
+    //   value: ROUTE_PATH.TOOLS,
+    //   type: 'route',
+    // },
   ];
   // if (['ordx.space'].every((v) => location.hostname !== v)) {
   //   items.push({
