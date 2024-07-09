8504f0708f8597e688d799bfb35d5cac4bd669c3
softwarecheng <softwarecheng@126.com>
2024-07-10 02:36:55 +0800

    fix

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# set -x
-set -e
+set -x
+# set -e
 git checkout main && git pull && git add . && git commit -m "fix"
 git checkout test && git pull && git merge main && npm run build:test && git add . && git commit -m "fix" && git push
 git checkout dev && git pull && git merge main && npm run build:dev && git add . && git commit -m "fix" && git push
