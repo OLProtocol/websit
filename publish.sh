2120b1f82471f9e7cedb27807fc8eeeb4ad74e9b
softwarecheng <softwarecheng@126.com>
2024-07-10 16:08:50 +0800

    fix

--- publish.sh
+++ publish.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -x
 # set -e
-git checkout main && git pull && git add . && git commit -m "fix"
-git checkout test && git pull && git merge main && npm run build:test && git add . && git commit -m "fix" && git push
+git checkout main && git pull && rm -rf dist && git add . && git commit -m "fix" && git push
+git checkout test && git pull && rm -rf dist && git add . && git commit -m "fix"
+git merge main && npm run build:test && git add . && git commit -m "fix" && git push
 git checkout dev && git pull && git merge main && npm run build:dev && git add . && git commit -m "fix" && git push
 git checkout main && npm run build:prod && git add . && git commit -m "fix" && git push
