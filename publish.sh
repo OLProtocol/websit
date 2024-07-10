de57f9692153c686d977153ad85b490d6f10ed50
softwarecheng <softwarecheng@126.com>
2024-07-10 16:16:36 +0800

    publish: clean main dist

--- publish.sh
+++ publish.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -x
 # set -e
-git checkout main && git pull && git add . && git commit -m "fix" && git push
-git checkout test && git pull && rm -rf dist && git add . && git commit -m "fix" && git push
-git merge main && npm run build:test && git add . && git commit -m "fix" && git push
-git checkout dev && git pull && git merge main && npm run build:dev && git add . && git commit -m "fix" && git push
-git checkout main && npm run build:prod && git add . && git commit -m "fix" && git push
+git checkout main && git pull && rm -rf dist && git add . && git commit -m "publish: clean main dist"
+git checkout test && git pull && rm -rf dist && git add . && git commit -m "publish: clean test dist"
+git checkout dev && git pull && rm -rf dist && git add . && git commit -m "publish: clean dev dist"
+
+git checkout test && git merge main && npm run build:test && git add . && git commit -m "publish: generate test dist" && git push
+git checkout dev && git merge main && npm run build:dev && git add . && git commit -m "publish: generate dev dist" && git push
+git checkout main && npm run build:prod && git add . && git commit -m "publish: generate prd dist" && git push
